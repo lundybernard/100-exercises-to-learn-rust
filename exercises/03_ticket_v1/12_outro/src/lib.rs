8ff8dba93637f99c3927c99d4262207d29c9169b
LundyBernard <lundy.bernard@gmail.com>
2024-08-26 17:36:42 -0700

    solve: (03) ticket_v1 - (12) outro

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,71 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(
+        product_name: String,
+        quantity: u32,
+        unit_price: u32
+    ) -> Self {
+        validate_product_name(&product_name);
+        validate_quantity(&quantity);
+        validate_unit_price(&unit_price);
+
+        Self{
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+
+    // Attributes
+    pub fn product_name(&self) -> &String { &self.product_name }
+    pub fn set_product_name(&mut self, new_product_name: String) {
+        validate_product_name(&new_product_name);
+        self.product_name = new_product_name
+    }
+
+    pub fn quantity(&self) -> &u32 { &self.quantity }
+    pub fn set_quantity(&mut self, new_quantity: u32) {
+        validate_quantity(&new_quantity);
+        self.quantity = new_quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 { &self.unit_price }
+    pub fn set_unit_price(&mut self, new_unit_price: u32) {
+        validate_unit_price(&new_unit_price);
+        self.unit_price = new_unit_price
+    }
+    // Computed values
+    pub fn total(&self) -> u32 { &self.quantity * &self.unit_price }
+}
+
+
+fn validate_product_name(product_name: &String) {
+    if product_name.is_empty() {
+        panic!("Title cannot be empty (null string)")
+    }
+    if product_name.len() > 300 {
+        panic!("Title cannot be longer than 300 bytes");
+    }
+}
+
+fn validate_quantity(quantity: &u32) {
+    if *quantity == 0 {
+        panic!("quantity must be greater than 0")
+    }
+}
+
+fn validate_unit_price(unit_price: &u32) {
+    if *unit_price == 0 {
+        panic!("unit price must be greater than 0")
+    }
+}
