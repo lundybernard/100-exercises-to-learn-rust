0399231974dcf3839e0fe83a3dbba7b2c84d7920
LundyBernard <lundy.bernard@gmail.com>
2024-09-03 16:31:12 -0700

    solve: (04) traits - (08) sized

--- exercises/04_traits/08_sized/src/lib.rs
+++ exercises/04_traits/08_sized/src/lib.rs
@@ -3,5 +3,16 @@ pub fn example() {
     // via `std::mem::size_of` will result in a compile-time error.
     //
     // TODO: Comment out the following line and move on to the next exercise.
-    std::mem::size_of::<str>();
+    // std::mem::size_of::<str>();
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sized_string_slice() {
+        assert_eq!(std::mem::size_of::<&str>(), 16);
+    }
 }
