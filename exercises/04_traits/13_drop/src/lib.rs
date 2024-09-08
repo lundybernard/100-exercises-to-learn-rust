0ee837d8cb51bb78ce4688d54b883fd6e12e1db6
LundyBernard <lundy.bernard@gmail.com>
2024-09-08 14:17:43 -0700

    solve: (04) traits - (13) drop

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,29 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+struct DropBomb {
+    armed: bool
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if self.armed {
+            panic!("You've set us up the bomb")
+        }
+    }
+}
+
+impl DropBomb {
+    pub fn new() -> Self {
+        Self{armed: true}
+    }
+    pub fn disarm(&mut self) {
+        self.armed = false
+    }
+}
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -16,7 +39,7 @@ mod tests {
     #[test]
     fn test_defused_drop_bomb() {
         let mut bomb = DropBomb::new();
-        bomb.defuse();
+        bomb.disarm();
         // The bomb should not panic when dropped
         // since it has been defused
     }
