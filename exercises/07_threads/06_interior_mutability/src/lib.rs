e5f38b56dedd7579ac2765e6c628322d70e59531
LundyBernard <lundy.bernard@gmail.com>
2024-12-05 13:43:22 -0800

    solve: (07) threads - (06) interior_mutability

--- exercises/07_threads/06_interior_mutability/src/lib.rs
+++ exercises/07_threads/06_interior_mutability/src/lib.rs
@@ -6,18 +6,20 @@ use std::rc::Rc;
 
 pub struct DropTracker<T> {
     value: T,
-    counter: todo!(),
+    counter: Rc<RefCell<usize>>,
 }
 
 impl<T> DropTracker<T> {
-    pub fn new(value: T, counter: todo!()) -> Self {
+    pub fn new(value: T, counter: Rc<RefCell<usize>>) -> Self {
         Self { value, counter }
     }
 }
 
 impl<T> Drop for DropTracker<T> {
     fn drop(&mut self) {
-        todo!()
+        // Increment the counter when the instance is dropped
+        let mut counter = self.counter.borrow_mut();
+        *counter += 1;
     }
 }
 
@@ -37,10 +39,20 @@ mod tests {
         let counter = Rc::new(RefCell::new(0));
 
         {
-            let a = DropTracker::new(5, Rc::clone(&counter));
-            let b = DropTracker::new(6, Rc::clone(&counter));
+            let _a = DropTracker::new(5, Rc::clone(&counter));
+            let _b = DropTracker::new(6, Rc::clone(&counter));
         }
 
         assert_eq!(*counter.borrow(), 2);
+    }
+
+    // add test for value attribute, to quiet compiler warning.
+    #[test]
+    fn value() {
+        let v: usize = 77;
+        let count = Rc::new(RefCell::new(0));
+        let drop_tracker = DropTracker::new(v, count);
+
+        assert_eq!(v, drop_tracker.value);
     }
 }
