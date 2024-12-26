8a4160609d139e520a543377daedd6c0fc95d12b
LundyBernard <lundy.bernard@gmail.com>
2024-12-26 15:33:58 -0800

    solve: (08) futures - (07) cancellation

--- exercises/08_futures/07_cancellation/src/lib.rs
+++ exercises/08_futures/07_cancellation/src/lib.rs
@@ -4,17 +4,29 @@ use std::time::Duration;
 use tokio::io::AsyncReadExt;
 use tokio::net::TcpListener;
 
-pub async fn run(listener: TcpListener, n_messages: usize, timeout: Duration) -> Vec<u8> {
+
+pub async fn run(
+    listener: TcpListener,
+    n_messages: usize,
+    timeout: Duration
+) -> Vec<u8> {
     let mut buffer = Vec::new();
+
     for _ in 0..n_messages {
-        let (mut stream, _) = listener.accept().await.unwrap();
-        let _ = tokio::time::timeout(timeout, async {
-            stream.read_to_end(&mut buffer).await.unwrap();
-        })
-        .await;
+        let (mut stream, _) = listener
+            .accept()
+            .await.unwrap();
+        let _ = tokio::time::timeout(
+            timeout,
+            async {
+                stream.read_to_end(&mut buffer)
+                    .await.unwrap();
+        }).await;
     }
+
     buffer
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -23,29 +35,46 @@ mod tests {
 
     #[tokio::test]
     async fn ping() {
-        let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+        let listener = TcpListener::bind("127.0.0.1:0")
+            .await.unwrap();
         let addr = listener.local_addr().unwrap();
         let messages = vec!["hello", "from", "this", "task"];
         let timeout = Duration::from_millis(20);
-        let handle = tokio::spawn(run(listener, messages.len(), timeout.clone()));
+        let handle = tokio::spawn(
+            run(listener, messages.len(), timeout.clone())
+        );
 
         for message in messages {
-            let mut socket = tokio::net::TcpStream::connect(addr).await.unwrap();
+            let mut socket = tokio::net::TcpStream::connect(addr)
+                .await.unwrap();
             let (_, mut writer) = socket.split();
 
+            // Here we slice-off the first 2 letters of each message
             let (beginning, end) = message.split_at(message.len() / 2);
 
             // Send first half
-            writer.write_all(beginning.as_bytes()).await.unwrap();
-            tokio::time::sleep(timeout * 2).await;
-            writer.write_all(end.as_bytes()).await.unwrap();
+            writer.write_all(beginning.as_bytes())
+                .await.unwrap();
+            // This sleep ensures that the test waits for the timeout to elapse
+            tokio::time::sleep(timeout * 2)
+                .await;
+            // we are able to call write_all again,
+            // even though the tasks will always have timed out
+            // and been canceled
+            writer.write_all(end.as_bytes())
+                .await.unwrap();
 
             // Close the write side of the socket
-            let _ = writer.shutdown().await;
+            let _ = writer.shutdown()
+                .await;
         }
 
-        let buffered = handle.await.unwrap();
-        let buffered = std::str::from_utf8(&buffered).unwrap();
-        assert_eq!(buffered, "");
+        let buffered = handle
+            .await.unwrap();
+        let buffered = std::str::from_utf8(&buffered)
+            .unwrap();
+        // Fix: Only the first part of each message is actually written
+        // the end is never written due to timing out
+        assert_eq!(buffered, "hefrthta");
     }
 }
